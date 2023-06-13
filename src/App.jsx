c008c383a5ee1a20df20cda566392a6160b7f559
lact03 <luistabug03@gmail.com>
2023-06-13 15:50:57 +0800

    add App.jsx file

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./index.css";
+import Header from "./components/Header";
+import MainContent from "./components/MainContent";
+import Footer from "./components/Footer";
+
+export default function App() {
+    return (
+        <>
+            <Header />
+            <MainContent />
+            <Footer />
+        </>
+    );
+}
