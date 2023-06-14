60a902de00982a3bf52d7d263ee584b1decf9b86
lact03 <luistabug03@gmail.com>
2023-06-14 15:14:56 +0800

    not working

--- src/components/MainContent.jsx
+++ src/components/MainContent.jsx
@@ -1,19 +1,29 @@
-import React, { useState, useEffect } from "react";
-import { useFetch } from "../API/fetchAPI";
-
 // gutendex.com/books
 // https://gutendex.com/
 // https://gutendex.com/books/?ids=1,2,3,4,5,6
 
+import React, { useState, useEffect } from "react";
+
 export default function MainContent() {
-  const [data, setData] = useState([]);
+  const [titles, setTitles] = useState([]);
+  const [cover, setCover] = useState([]);
 
   const apiGet = async () => {
-    const response = await fetch("https://gutendex.com/books");
-    const booksObject = await response.json();
-    const books = booksObject.results;
+    try {
+      const response = await fetch("https://gutendex.com/books");
+      const booksObject = await response.json();
+      const books = booksObject.results;
 
-    setData(books);
+      const fetchedTitles = books.map((book) => book.title);
+      const fetchedCovers = books.map(
+        (book) => book.format && book.format["image/jpeg"]
+      );
+
+      setTitles(fetchedTitles);
+      setCover(fetchedCovers);
+    } catch (error) {
+      console.error("Error fetching API:", error);
+    }
   };
 
   useEffect(() => {}, []);
@@ -25,8 +35,13 @@ export default function MainContent() {
       <br />
       <div>
         <ul>
-          {data.map((item) => (
-            <li>{item.title}</li>
+          {titles.map((title, index) => (
+            <div key={index}>
+              <li>{title}</li>
+            </div>
+          ))}
+          {cover.map((cover, index) => (
+            <img key={index} src={cover} alt="Book cover" />
           ))}
         </ul>
       </div>
