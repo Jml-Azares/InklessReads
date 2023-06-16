0990354c7c899e3e0ddcee60a6d83a8d20d38b11
Joseph Moroni L. Azares <133185546+Jml-Azares@users.noreply.github.com>
2023-06-16 16:01:42 +0800

    added hero to layout

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,11 +1,9 @@
 import React from "react";
-import Hero from "./Hero";
 import MainContent from "./MainContent";
 
 const Home = () => {
   return (
     <div>
-      <Hero />
       <MainContent
         picks="Featured Picks"
         api="https://gutendex.com/books?sort=popular"
--- src/components/PrivacyPolicy.jsx
+++ src/components/PrivacyPolicy.jsx
@@ -5,7 +5,6 @@ function PrivacyPolicy() {
     <div className="p-4">
       <h1>Privacy Policy for Inkless Reads</h1>
       <p>Effective Date: [Insert Date]</p>
-
       <p>
         At Inkless Reads, we value the privacy of our users and are committed to
         protecting their personal information. This Privacy Policy outlines how
@@ -14,21 +13,18 @@ function PrivacyPolicy() {
         Inkless Reads, you consent to the terms described in this Privacy
         Policy.
       </p>
-
       <h2>1. Information We Collect:</h2>
       <p>
         <strong>1.1 Personal Information:</strong> When you create an account or
         make a purchase on Inkless Reads, we may collect personal information
         such as your name, email address, billing address, and payment details.
       </p>
-
       <p>
         <strong>1.2 Log Data:</strong> We collect information that your browser
         sends whenever you visit our website. This data may include your IP
         address, browser type, browser version, pages of our website visited,
         the time and date of your visit, and other statistics.
       </p>
-
       <p>
         <strong>1.3 Cookies:</strong> We may use cookies and similar
         technologies to enhance your browsing experience and gather information
@@ -36,7 +32,6 @@ function PrivacyPolicy() {
         cookies, but please note that certain features of our website may not
         function properly as a result.
       </p>
-
       <h2>2. Use of Collected Information:</h2>
       <p>
         <strong>2.1 Provide and Improve Services:</strong> We use the
@@ -44,20 +39,17 @@ function PrivacyPolicy() {
         processing payments, delivering ebooks, personalizing your experience,
         and responding to your inquiries and requests.
       </p>
-
       <p>
         <strong>2.2 Communication:</strong> We may use your contact information
         to communicate with you about your account, promotional offers, updates,
         and other relevant information related to Inkless Reads. You can opt out
         of these communications at any time.
       </p>
-
       <p>
         <strong>2.3 Analytics:</strong> We may use collected data for analytics
         purposes, such as analyzing user behavior, improving our website and
         services, and measuring the effectiveness of our marketing efforts.
       </p>
-
       <h2>3. Data Sharing and Disclosure:</h2>
       <p>
         <strong>3.1 Third-Party Service Providers:</strong> We may share your
@@ -67,20 +59,17 @@ function PrivacyPolicy() {
         providers are obligated to handle your information securely and
         confidentially.
       </p>
-
       <p>
         <strong>3.2 Legal Compliance:</strong> We may disclose your information
         if required by law, to comply with legal processes, protect our rights,
         or respond to legal requests and investigations.
       </p>
-
       <p>
         <strong>3.3 Business Transfers:</strong> In the event of a merger,
         acquisition, or sale of all or a portion of our assets, your information
         may be transferred as part of the transaction. We will notify you before
         your personal information becomes subject to a different privacy policy.
       </p>
-
       <h2>4. Data Security:</h2>
       <p>
         We employ industry-standard security measures to protect your personal
@@ -90,7 +79,6 @@ function PrivacyPolicy() {
         commercially acceptable means to protect your data, we cannot guarantee
         its absolute security.
       </p>
-
       <h2>5. Children's Privacy:</h2>
       <p>
         Inkless Reads is not intended for use by individuals under the age of
@@ -99,7 +87,6 @@ function PrivacyPolicy() {
         unintentionally collected, we will take prompt steps to delete it from
         our systems.
       </p>
-
       <h2>6. Changes to this Privacy Policy:</h2>
       <p>
         We reserve the right to update or modify this Privacy Policy at any
@@ -107,13 +94,11 @@ function PrivacyPolicy() {
         policy on our website or through other communication channels. Please
         review this policy periodically to stay informed of any updates.
       </p>
-
       <h2>7. Contact Us:</h2>
       <p>
         If you have any questions, concerns, or requests regarding this Privacy
         Policy, please contact us at [insert contact information].
       </p>
-
       <p>
         By using Inkless Reads, you acknowledge that you have read, understood,
         and agreed to this Privacy Policy.
