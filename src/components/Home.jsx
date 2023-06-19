7a03e830ad25c0abf3cc28d503602a8dba21287b
lact03 <luistabug03@gmail.com>
2023-06-19 14:47:10 +0800

    none

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,7 +4,10 @@ import MainContent from "./MainContent";
 const Home = () => {
   return (
     <div>
-      <MainContent picks="Featured Picks" api="https://gutendex.com/books" />
+      <MainContent
+        picks="Featured Picks"
+        api="https://gutendex.com/books/?ids=1513,2701,145,2641,67979,100"
+      />
       <MainContent
         picks="Top Picks"
         api="https://gutendex.com/books/?ids=394,2160,6761,4085,1259,6593"
