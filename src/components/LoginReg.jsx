78352992f02a91089af3c4f5b3e473374a544e5c
Joseph Moroni L. Azares <133185546+Jml-Azares@users.noreply.github.com>
2023-06-19 18:18:39 +0800

    clear error on console

--- src/components/LoginReg.jsx
+++ src/components/LoginReg.jsx
@@ -166,8 +166,12 @@ function LoginReg() {
             <div className="row">
               {/* Country */}
               <div className="col-6">
-                <select id="countrySelect" className="form-select">
-                  <option value="" disabled selected>
+                <select
+                  id="countrySelect"
+                  className="form-select"
+                  defaultValue=""
+                >
+                  <option value="" disabled>
                     -Select Country-
                   </option>
                   {countries.map((country) => (
