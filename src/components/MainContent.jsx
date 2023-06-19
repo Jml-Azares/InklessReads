0527dbfbc828aff3fa41abc0893e77555cd80bb8
lact03 <luistabug03@gmail.com>
2023-06-19 11:53:44 +0800

    add book details

--- src/components/MainContent.jsx
+++ src/components/MainContent.jsx
@@ -1,31 +1,13 @@
 // https://gutendex.com/books
 // https://gutendex.com/
-// https://gutendex.com/books/?ids=1,2,3,4,5,6
+// https://gutendex.com/books/?ids=1513,2701,145,2641,67979,100
 
-import React, { useState, useEffect } from "react";
+import useFetch from "../API/useFetch";
+import { Link } from "react-router-dom";
 import "./MainContent.css";
 
-export default function MainContent({ picks, api, start, end }) {
-  const [data, setData] = useState([]);
-  const [loading, setLoading] = useState(true);
-
-  const apiGet = async () => {
-    try {
-      const response = await fetch(api);
-      const booksObject = await response.json();
-      const books = booksObject.results;
-      const booksFiltered = books.slice(start, end);
-
-      setData(booksFiltered);
-      setLoading(false);
-    } catch (error) {
-      console.error("Error fetching API:", error);
-    }
-  };
-
-  useEffect(() => {
-    apiGet();
-  }, []);
+export default function MainContent({ picks, api }) {
+  const { data, loading, error } = useFetch(api);
 
   return (
     <div className="container-fluid">
@@ -36,6 +18,7 @@ export default function MainContent({ picks, api, start, end }) {
           </h2>
         </div>
         <div className="row px-5">
+          {error && <div>{error}</div>}
           {loading && (
             <h4 className="col-12" style={{ textAlign: "center" }}>
               Loading...
@@ -44,11 +27,13 @@ export default function MainContent({ picks, api, start, end }) {
           {data &&
             data.map((item) => (
               <div
-                className="card-parent-div col-12 col-md-4 col-lg-2 px-2"
+                className="card-parent-div col-12 col-md-4 col-lg-2 p-3"
                 key={item.id}
               >
                 <div className="card-img p-1">
-                  <img src={item.formats["image/jpeg"]} />
+                  <Link to={`/book/${item.id}`}>
+                    <img src={item.formats["image/jpeg"]} />
+                  </Link>
                 </div>
               </div>
             ))}
