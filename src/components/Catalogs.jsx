b30be5c48203acd6ac73286a3f89cad4f4e469ca
lact03 <luistabug03@gmail.com>
2023-06-29 18:41:08 +0800

    not done yet on sort

--- src/components/Catalogs.jsx
+++ src/components/Catalogs.jsx
@@ -1,10 +1,11 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import useFetch from "../API/useFetch";
 import { Link } from "react-router-dom";
 import "./catalog.css";
 
 function Catalog({ api, catalog }) {
   const { data, loading, error } = useFetch(api);
+  const [books, setBooks] = useState([]);
   const [hoveredItem, setHoveredItem] = useState(null);
 
   const handleMouseEnter = (itemId) => {
@@ -15,13 +16,51 @@ function Catalog({ api, catalog }) {
     setHoveredItem(null);
   };
 
+  useEffect(() => {
+    setBooks(data);
+  }, [data]);
+
+  const compareByProperty = (a, b, property) => {
+    if (a[property] && b[property]) {
+      console.log("hello World");
+    } else if (a[property]) {
+      console.log("hello World");
+    } else if (b[property]) {
+      console.log("hello World");
+    } else {
+      console.log("hello World");
+    }
+  };
+
+  const sort = (criteria) => {
+    const sortedBooks = [...books];
+
+    if (criteria === "title") {
+      sortedBooks.sort((a, b) => a.title.localeCompare(b.title));
+      setBooks(sortedBooks);
+    } else if (criteria === "author") {
+      sortedBooks.sort((a, b) => {
+        compareByProperty(a.authors[0], b.authors[0], "name");
+      });
+      setBooks(sortedBooks);
+    }
+  };
+
   return (
     <div className="container-fluid">
       <div className="row my-5">
-        <div className="row px-4">
-          <h2 className="col-12 pb-5" style={{ textAlign: "left" }}>
-            {catalog}
-          </h2>
+        <div className="px-4 d-flex justify-content-between">
+          <h2 className="">{catalog}</h2>
+          <select
+            className=""
+            onChange={(e) => {
+              sort(e.target.value);
+            }}
+          >
+            <option value="">--Sort--</option>
+            <option value="title">Title</option>
+            <option value="author">Author</option>
+          </select>
         </div>
         <div className="row m-0">
           {error && <div>{error}</div>}
@@ -32,8 +71,8 @@ function Catalog({ api, catalog }) {
               </div>
             </div>
           )}
-          {data &&
-            data.map((item) => (
+          {books &&
+            books.map((item) => (
               <div
                 className={`gallery-card-parent-div col-4 col-lg-2 p-1 p-md-4 ${
                   hoveredItem === item.id ? "hovered" : ""
