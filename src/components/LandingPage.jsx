42d5d32eb1304b0674ab87f894c5c72b6dc5c9c5
DantechJr <dantecalcantarajr@gmail.com>
2023-07-25 02:43:41 +0800

    landing page tagline update

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -8,7 +8,7 @@ import { createUserWithEmailAndPassword } from "firebase/auth";
 import { Navigate, useNavigate } from "react-router";
 // import "firebase/compat/auth";
 // import "firebase/database";
-import "firebase/database";
+// import "firebase/database";
 import Footer from "./Footer";
 import axios from "axios";
 import { StateContext } from "../contexts/ContextProvider";
@@ -576,13 +576,13 @@ function LandingPage() {
                 />
               </div>
               <h1 className="fw-bold text-center text-body-emphasis mb-3">
-                GUSTO KO NG SINANGAG!
+                Inkless Reads: Unleash the Magic of Words
               </h1>
               <p
                 className="text-dark col-lg-10 fs-4"
                 style={{ textAlign: "center" }}
               >
-                Pinirotong tampal-puke
+                Your Online Bookstore for Endless Adventures!
               </p>
             </div>
 
@@ -975,6 +975,7 @@ function LandingPage() {
                             <input
                               className="btn btn-lg btn-outline-dark w-100"
                               type="submit"
+                              data-bs-dismiss="modal"
                             />
                           </div>
                         </div>
